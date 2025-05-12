e79a5455ae84fe3fd51ee4495f88fdf00f26c887
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-05-12 13:29:49 -0700

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  defineConfig([
+    {
+      files: ["**/*.{js,mjs,cjs}"],
+      plugins: { js, eslintConfigPrettier },
+      extends: ["js/recommended", "prettier"],
+    },
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    {
+      files: ["**/*.{js,mjs,cjs}"],
+      languageOptions: { globals: globals.node },
+    },
+  ]),
+  eslintConfigPrettier,
+];
