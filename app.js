dff33004d45e651c92d63168ab64327ffcd84c35
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-05-12 15:26:44 -0700

    Added cors

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-const indexRouter = require("./routes/index");
+const indexRouter = require('./routes/index');
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(cors);
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
 // Catch 404 errors and forward to error handler
 app.use((req, res, next) => {
-  const error = new Error("Not Found");
+  const error = new Error('Not Found');
   error.status = 404;
   next(error);
 });
