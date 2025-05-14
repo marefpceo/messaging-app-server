d07b2a301dfa8b1cc714bfc1fc956d9fdbd9f3fe
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-05-13 20:39:27 -0700

    Renamed authController to messageController

---
+++ controllers/messageController.js
@@ -0,0 +1,43 @@
+const asyncHandler = require('express-async-handler');
+
+exports.create_message_get = asyncHandler(async (req, res, next) => {
+  res.json({
+    message: 'Create Message GET',
+  });
+});
+
+exports.create_message_post = asyncHandler(async (req, res, next) => {
+  res.json({
+    message: 'Create Message POST',
+  });
+});
+
+exports.conversation_list_get = asyncHandler(async (req, res, next) => {
+  res.json({
+    message: 'Conversation List GET',
+  });
+});
+
+exports.consersation_get = asyncHandler(async (req, res, next) => {
+  res.json({
+    message: 'Conversation GET',
+  });
+});
+
+exports.conversation_delete = asyncHandler(async (req, res, next) => {
+  res.json({
+    message: 'Conversation DELETE',
+  });
+});
+
+exports.message_get = asyncHandler(async (req, res, next) => {
+  res.json({
+    message: 'Message GET',
+  });
+});
+
+exports.message_delete = asyncHandler(async (req, res, next) => {
+  res.json({
+    message: 'Message DELETE',
+  });
+});
