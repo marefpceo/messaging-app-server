eae80f5716612b2e9e2a6a8dce8367391b09610a
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-05-21 04:50:18 -0700

    Removed unused functions from indexController

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,17 +1,5 @@
 const asyncHandler = require('express-async-handler');
 
-exports.getIndex = asyncHandler(async (req, res, next) => {
-  res.json({
-    message: 'mCHAT',
-  });
-});
-
-exports.signup_get = asyncHandler(async (req, res, next) => {
-  res.json({
-    message: 'signup GET route',
-  });
-});
-
 exports.signup_post = asyncHandler(async (req, res, next) => {
   res.json({
     message: 'signup POST route',
