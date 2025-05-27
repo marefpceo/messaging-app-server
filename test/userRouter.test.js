c61fab0c39dc203fb01c179841f5e22411851f37
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-05-26 17:06:09 -0700

    Created setup and teardown functions for user edit

--- test/userRouter.test.js
+++ test/userRouter.test.js
@@ -1,4 +1,6 @@
 const userRouter = require('../routes/userRouter');
+const { PrismaClient } = require('../generated/prisma');
+const prisma = new PrismaClient();
 
 const request = require('supertest');
 const express = require('express');
@@ -7,14 +9,45 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use('/user', userRouter);
 
-// Test GET User Profile
-describe('GET /user/:user/edit_profile', () => {
-  it('get user profile route works', (done) => {
-    request(app)
-      .get('/user/:user/edit_profile')
-      .expect('Content-Type', /json/)
-      .expect({ message: 'Edit Profile GET' })
-      .expect(200, done);
+// Test all user routes\
+describe('Test user routes used to edit the user profile', () => {
+  let userId = '';
+  const testUser = {
+    firstname: 'User',
+    lastname: 'Router',
+    date_of_birth: '2025-05-26',
+    email: 'UserRouter@test.com',
+    password: 'kkkkkkkkk',
+  };
+
+  beforeAll(async () => {
+    user = await prisma.user.create({
+      data: testUser,
+    });
+    userId = user.id;
+  });
+
+  afterAll(async () => {
+    await prisma.user.delete({
+      where: {
+        email: testUser.email,
+      },
+    });
+  });
+
+  // Test GET user profile
+  it('get user profile route works', async () => {
+    const response = request(app).get(`/user/${userId}/edit_profile`);
+    expect(response.body).toEqual({
+      firstname: testUser.firstname,
+      lastname: testUser.lastname,
+      date_of_birth: testUser.date_of_birth,
+      email: testUser.email,
+      bio: 'profile bio',
+      bg_setting: 'black',
+      font_setting: 'large',
+      color: 'white',
+    });
   });
 });
 
