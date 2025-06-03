a16ebb3fa383889aaaaf535ceaf11648de31a206
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-06-03 15:26:11 -0700

    Completed contacts_get to return global list

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,9 +1,26 @@
 const asyncHandler = require('express-async-handler');
+const { PrismaClient } = require('../generated/prisma');
+const prisma = new PrismaClient();
 
+// GET global list of all contacts
 exports.contacts_get = asyncHandler(async (req, res, next) => {
-  res.json({
-    message: 'Contacts GET',
+  const verifyList = await prisma.user.findMany({
+    include: {
+      profile: {
+        include: {
+          settings: true,
+        },
+      },
+    },
   });
+
+  if (!verifyList) {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  } else {
+    res.status(200).json(verifyList);
+  }
 });
 
 exports.add_contact_post = asyncHandler(async (req, res, next) => {
