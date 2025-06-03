379b9e088c5de64049a4e2eb9eb8df1a7e632ba5
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-06-03 15:56:35 -0700

    Completed functionality to add contact to user

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -23,12 +23,35 @@ exports.contacts_get = asyncHandler(async (req, res, next) => {
   }
 });
 
+// Add contact to current user
 exports.add_contact_post = asyncHandler(async (req, res, next) => {
-  const testResponse = {
-    name: req.body.name,
-    email: req.body.email,
-  };
-  res.json(testResponse);
+  const currentUser = await prisma.user.findUnique({
+    where: {
+      username: req.body.currentUser,
+    },
+  });
+  const userToAdd = await prisma.user.findUnique({
+    where: {
+      username: req.body.addUser,
+    },
+  });
+
+  if (userToAdd === null || currentUser === null) {
+    res.status(404).json({
+      message: 'User not found',
+    });
+    return;
+  } else {
+    await prisma.contact.create({
+      data: {
+        userId: currentUser.id,
+        contactUserId: userToAdd.id,
+      },
+    });
+  }
+  res.json({
+    message: `${userToAdd.username} added!`,
+  });
 });
 
 exports.contact_delete = asyncHandler(async (req, res, next) => {
