9ba592b08f47ecf39127715487d22899329517b5
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-11-05 08:17:19 -0800

    Added preview URL to cors

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(
           "'self'",
           'http://localhost:3000',
           'http://localhost:5173',
+          'http://localhost:4173',
           /.*\.railway.app.*/,
         ],
       },
@@ -67,7 +68,7 @@ app.use(
     cookie: {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       secure: process.env.NODE_ENV === 'production' ? true : false,
-      sameSite: process.env.NODE_ENV === 'development' ? 'none' : 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     },
     secret: process.env.SESSION_SECRET,
     resave: true,
