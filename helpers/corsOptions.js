d0ea4245da1c835d7970fe7395b30c020c235f54
marefpceo <32721274+marefpceo@users.noreply.github.com>
2025-11-04 17:31:59 -0800

    Updated corsOptions file

--- helpers/corsOptions.js
+++ helpers/corsOptions.js
@@ -1,3 +1,7 @@
-const allowedOrigins = [/http:\/\/localhost:3000/, /http:\/\/localhost:5173/];
+const allowedOrigins = [
+  /http:\/\/localhost:3000/,
+  /http:\/\/localhost:5173/,
+  /.*\.railway.app.*/,
+];
 
 module.exports = allowedOrigins;
